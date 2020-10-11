7c558bd93af3d5598e79bc0448bb58d17f471ac7
Evyatar Daud <evyatar.daud@gmail.com>
2020-10-11 14:35:49 +0300

    Updates

---
+++ precache-manifest.32c8b7b1c99a9e232179dee66f4be8d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473653777459a84567c4d6888dd4ea57",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6165aa3427966ac9c38",
+    "url": "./static/css/main.70ed793b.chunk.css"
+  },
+  {
+    "revision": "9f708a08410782f4ee56",
+    "url": "./static/js/2.bdde2880.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bdde2880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6165aa3427966ac9c38",
+    "url": "./static/js/main.6eedb3fa.chunk.js"
+  },
+  {
+    "revision": "83130f59d7850caacbf6",
+    "url": "./static/js/runtime-main.1f8d02b9.js"
+  }
+]);
