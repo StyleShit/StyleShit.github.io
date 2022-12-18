5f111535afd8348bff62cbf7bcef249f79f1c9c9
StyleShit <evyatar.daud@gmail.com>
2022-12-18 15:00:23 +0200

    Updates

--- _next/static/S3pSK8RKEbjeQga1jhe8h/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/171-f38a6dc560735553.js","static/chunks/pages/index-e643342790b21eef.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/TTuiNQ3vhleb-hNEwA5Lg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/171-f38a6dc560735553.js","static/chunks/pages/index-146231eaa946f5c7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
